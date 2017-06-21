1047e5b3355169f72fee5964580d50c09d91878b
Thomas Alberola <thomas.alberola@ekino.fr>
2017-06-21 11:10:16 +0200

    Fix for emoji on android

--- src/components/0_Welcome/Welcome.js
+++ src/components/0_Welcome/Welcome.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './welcome.css'
+import { getEmojiSize } from '../../utils/utils'
 
 const Welcome = (props) => (
   <section className="Welcome">
             <span>
               <h1 className="Welcome__verb">Welcome</h1>
               <h2 className="Welcome__name">{props.currentUser.name}</h2>
-              <div className="Welcome__emoji">{props.currentUser.emoji}</div>
+              <div style={{ fontSize: getEmojiSize() }} className="Welcome__emoji">{props.currentUser.emoji}</div>
             </span>
   </section>
 )
+
 
 export default Welcome
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -45,6 +45,32 @@ export const findStoredUserByUid = (loggedUser, users) => {
   return (users.filter(user => user.uid === loggedUser))[0]
 }
 
+export const getOS = () => {
+  let userAgent = window.navigator.userAgent,
+    platform = window.navigator.platform,
+    macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+    windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+    iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+    os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
+
+export const getEmojiSize = () => getOS() === 'Android' ? '54px' : '150px'
+
+
 export const getUsers = (base) => (
   base.fetch('users', {
     context: this,
